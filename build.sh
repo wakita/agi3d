ed2196715e6dfeb89619924a6b379302f21ce684
takami9 <takami.m.aa@m.titech.ac.jp>
2013-01-30 11:39:05 +0900

    update

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-g++ -O3 -I lib -framework OpenGL -framework GLUT src/render.cpp -llapack -lblas  src/calcLayout.cpp lib/liblbfgs.dylib src/reprojection.cpp
+g++ -O3 -DNDEBUG -I lib -framework OpenGL -framework GLUT src/render.cpp -llapack -lblas  src/calcLayout.cpp lib/liblbfgs.dylib src/reprojection.cpp
